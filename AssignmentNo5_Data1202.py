e02ca4ff10807192672b0f04b6c7bcabf942568a
dimplpar <103013047+dimplpar@users.noreply.github.com>
2022-04-04 23:14:43 -0400

    Update AssignmentNo5_Data1202.py

--- AssignmentNo5_Data1202.py
+++ AssignmentNo5_Data1202.py
@@ -1,1 +1,13 @@
-#Create new connection
+# Import pandas
+import pandas as pd
+
+# Set file path to a variable
+input_file_path = r"C:\Users\Durham\Desktop\Duraham\Courses\data1202\vgsales.csv"
+
+# Read Data into a DataFrame
+input_raw_data = pd.read_csv(input_file_path)
+
+# Filter DataFrame for proper years
+between_00_05 = input_raw_data[(input_raw_data["Year"] >= 2000) & (input_raw_data["Year"] <= 2005)]
+
+print(between_00_05)
