c4dfbe3320dae9a9265d9257653092f269c04981
dimplpar <103013047+dimplpar@users.noreply.github.com>
2022-04-05 00:14:45 -0400

    Update AssignmentNo5_Data1202.py

--- AssignmentNo5_Data1202.py
+++ AssignmentNo5_Data1202.py
@@ -1,3 +1,4 @@
+#PART 1
 # Import pandas
 import pandas as pd
 
@@ -11,3 +12,39 @@ input_raw_data = pd.read_csv(input_file_path)
 between_00_05 = input_raw_data[(input_raw_data["Year"] >= 2000) & (input_raw_data["Year"] <= 2005)]
 
 print(between_00_05)
+
+#PART 2
+# How to create an engine and connect to a database
+
+# import required libraries
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+
+# create engine
+engine = create_engine('mysql+pymysql://database:username@localhost/data1202')
+
+# connection string
+conn = engine.connect()
+
+# read a simple query into DataFrame
+df = pd.read_sql("SELECT * FROM data1202.vgsales_new", conn)
+
+# print DataFrame
+print(df.head())
+
+# read a complex query into DataFrame
+complex_df = pd.read_sql('''SELECT
+    Round(SUM(NA_Sales)) as 'NA_Sales',
+    ROUND(SUM(EU_Sales)) as 'EU_Sales',
+    ROUND(SUM(JP_Sales)) as 'JP_Sales',
+    ROUND(SUM(Global_Sales)) AS 'Global_Sales',
+    ROUND((SUM(NA_Sales)/SUM(Global_Sales)) * 100) as 'NA_GlobalShare'
+
+FROM
+    data1202.vgsales_new
+WHERE
+    Genre = 'Action'
+        AND Year>= 2005''', conn)
+
+print(complex_df)
